5e702ad195a21abb139b261baeecc6566c632b57
Aaron <aaron@ironmthome.com>
2020-04-05 01:19:33 -0400

    Combined bootstrap.js + app.js -> app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
-require('./bootstrap');
+window._ = require('lodash');
+window.axios = require('axios');
 
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
 import Vue from 'vue';
 import router from '@/js/routes.js';
 import store from '@/js/store.js';
