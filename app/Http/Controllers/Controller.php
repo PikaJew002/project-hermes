64fb3526c05bd1459fe6218e69ced5e1dcd32fe5
Aaron <aaron@ironmthome.com>
2020-04-05 01:14:39 -0400

    Added parseOptions func to base-controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,26 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Parse query string to extract options.
+     *
+     * @return array
+     */
+    public function parseOptions(Request $request) {
+        $query = $request->query();
+        $options = [
+            'with' => []
+        ];
+        if(isset($query['query'])) {
+            $options['with'] = explode(':', $query['with']);
+        }
+
+        return $options;
+    }
 }
